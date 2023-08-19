9a56ba7def9bb63733d7b3c4af40aa277bbe684c
cylin577 <78150792+cylin577@users.noreply.github.com>
2023-08-19 13:27:35 +0800

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,4 +1,12 @@
+#! bin/bash
+
+#this program need root 
+
+echo "this program need root "
+echo "preparing update"
+sleep 5
 sudo apt-get update
+echo "updateing"
 sudo apt-get upgrade
-echo update is done please reboot
+echo "update is done please reboot"
 exit
